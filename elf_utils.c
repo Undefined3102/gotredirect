d77e69861bfc6a73169d9e557b408b492c898a91
Vasiliy Demin <undefined3102@gmail.com>
2018-10-29 19:11:51 +0300

    Delete find_symbol_rel and find_symbol. Add section_by_name, section_by_type, rel_by_name, symbol_by_name. Rename elf_check to lib_check. Move mmap_read_file to got_redirect.c

--- elf_utils.c
+++ elf_utils.c
@@ -1,63 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
 #include <elf.h>
 
 #include "elf_utils.h"
 #include "operation.h"
 
-void* mmap_read_file(const char *filename, uint32_t *size) {
-    int fd;
-    struct stat f_info;
-    void *mem;
-
-    mem = MAP_FAILED;
-
-    fd = open(filename, O_RDONLY);
-    if(fd < 0) {
-        perror("open");
-        goto cleanup;
-    }
-
-    if(fstat(fd, &f_info) < 0) {
-        perror("fstat");
-        goto cleanup;
-    }
-
-    mem = mmap(NULL, f_info.st_size, PROT_READ, MAP_PRIVATE, fd, 0); 
-    if(mem == MAP_FAILED) {
-        perror("mmap");
-        goto cleanup;
-    }
-
-    *size = f_info.st_size;
-cleanup:
-    if(fd >= 0) close(fd);
-    return (mem == MAP_FAILED ? NULL : mem);
-}
-
-Elf32_Shdr* find_section(uint8_t *mem, const char *sh_name) {
+Elf32_Shdr* section_by_type(uint8_t *mem, Elf32_Word sh_type) {
     Elf32_Ehdr *ehdr = (Elf32_Ehdr*)mem;
     Elf32_Shdr *shdr = (Elf32_Shdr*)(mem + ehdr->e_shoff);
-    char *strtab = (char*)&mem[shdr[ehdr->e_shstrndx].sh_offset];
     int i;
 
     for(i = 0; i < ehdr->e_shnum; i++) {
-        if(strcmp(&strtab[shdr[i].sh_name], sh_name) == 0)
-            return &shdr[i];
+        if(shdr->sh_type == sh_type)
+            return shdr;
+        shdr++;
     }
 
     return NULL;
 }
 
-Elf32_Rel* find_symbol_rel(uint8_t *mem, const char *sh_name, const char *sym_name) {
+
+Elf32_Shdr* section_by_name(uint8_t *mem, const char *sh_name) {
+    Elf32_Ehdr *ehdr = (Elf32_Ehdr*)mem;
+    Elf32_Shdr *shdr = (Elf32_Shdr*)(mem + ehdr->e_shoff);
+    char *strtab     = (char*)&mem[shdr[ehdr->e_shstrndx].sh_offset];
+    int i;
+
+    for(i = 0; i < ehdr->e_shnum; i++) {
+        if(strcmp(strtab + shdr->sh_name, sh_name) == 0)
+            return shdr;
+        shdr++;
+    }
+
+    return NULL;
+}
+
+
+Elf32_Rel* rel_by_name(uint8_t *mem, const char *sym_name) {
     Elf32_Ehdr *ehdr = (Elf32_Ehdr*)mem;
     Elf32_Shdr *shdr = (Elf32_Shdr*)(mem + ehdr->e_shoff);
 
-    Elf32_Shdr *sh_rel = find_section(mem, sh_name);
+    Elf32_Shdr *sh_rel = section_by_name(mem, ".rel.plt");
     if(sh_rel == NULL || sh_rel->sh_type != SHT_REL)
         return NULL;
 
@@ -69,20 +52,21 @@ Elf32_Rel* find_symbol_rel(uint8_t *mem, const char *sh_name, const char *sym_na
     int i, index;
 
     for(i = 0; i < rel_count; i++) {
-        index = ELF32_R_SYM(reltab[i].r_info);
+        index = ELF32_R_SYM(reltab->r_info);
         if(strcmp(&strtab[symtab[index].st_name], sym_name) == 0)
-            return &reltab[i];
+            return reltab;
+        reltab++;
     }
 
     return NULL;
 }
 
-Elf32_Sym* find_symbol(uint8_t *mem, const char *sh_name, const char *sym_name) {
+Elf32_Sym* symbol_by_name(uint8_t *mem, const char *sym_name) {
     Elf32_Ehdr *ehdr = (Elf32_Ehdr*)mem;
     Elf32_Shdr *shdr = (Elf32_Shdr*)(mem + ehdr->e_shoff);
 
-    Elf32_Shdr *sh_sym = find_section(mem, sh_name);
-    if(sh_sym == NULL || sh_sym->sh_type != SHT_DYNSYM)
+    Elf32_Shdr *sh_sym = section_by_type(mem, SHT_DYNSYM);
+    if(sh_sym == NULL)
         return NULL;
 
     Elf32_Sym *symtab = (Elf32_Sym*)(mem + sh_sym->sh_offset);
@@ -91,15 +75,15 @@ Elf32_Sym* find_symbol(uint8_t *mem, const char *sh_name, const char *sym_name) 
     int i;
 
     for(i = 0; i < sym_count; i++) {
-        if(strcmp(&strtab[symtab[i].st_name], sym_name) == 0)
-            return &symtab[i];
+        if(strcmp(&strtab[symtab->st_name], sym_name) == 0)
+            return symtab;
+        symtab++;
     }
 
     return NULL;
 }
 
-
-int elf_check(uint8_t *lib_mem, const char *filename) {
+int lib_check(uint8_t *lib_mem, const char *filename) {
     Elf32_Ehdr *ehdr = (Elf32_Ehdr*)lib_mem;
 
     if(memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0) {
@@ -121,12 +105,11 @@ int elf_check(uint8_t *lib_mem, const char *filename) {
 int grab_operation_info(uint8_t *exec_mem, uint8_t *lib_mem, uint32_t e_base_addr, operation *op) {
     Elf32_Ehdr *ehdr = (Elf32_Ehdr*)exec_mem;
 
-    Elf32_Rel *rel = find_symbol_rel(exec_mem, ".rel.plt", op->orig_func);
-    rel = rel == NULL ? find_symbol_rel(exec_mem, ".rel.dyn", op->orig_func) : rel;
+    Elf32_Rel *rel = rel_by_name(exec_mem, op->orig_func);
     if(rel == NULL)
         return -1;
 
-    Elf32_Sym *sym = find_symbol(lib_mem, ".dynsym", op->repl_func);
+    Elf32_Sym *sym = symbol_by_name(lib_mem, op->repl_func);
     if(sym == NULL)
         return -1;
 
--- elf_utils.h
+++ elf_utils.h
@@ -4,10 +4,11 @@
 
 #include "operation.h"
 
-void* mmap_read_file(const char *filename, uint32_t *size);
-Elf32_Shdr* find_section(uint8_t *mem, const char *sh_name);
-Elf32_Rel* find_symbol_rel(uint8_t *mem, const char *sh_name, const char *sym_name);
-Elf32_Sym* find_symbol(uint8_t *mem, const char *sh_name, const char *sym_name);
+Elf32_Shdr* section_by_type(uint8_t *mem, Elf32_Word sh_type);
+Elf32_Shdr* section_by_name(uint8_t *mem, const char *sh_name);
 
-int elf_check(uint8_t *lib_mem, const char *filename);
+Elf32_Rel* rel_by_name(uint8_t *mem, const char *sym_name);
+Elf32_Sym* symbol_by_name(uint8_t *mem, const char *sym_name);
+
+int lib_check(uint8_t *lib_mem, const char *filename);
 int grab_operation_info(uint8_t *exec_mem, uint8_t *lib_mem, uint32_t e_base_addr, operation *op);
